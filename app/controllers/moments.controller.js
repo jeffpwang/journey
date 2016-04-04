69c85f4cfc5d15ab4c7e55eacc3074a3358bafed
dgilbs <danielgilberg@gmail.com>
2016-04-04 16:38:17 -0400

    adds panels for moments and color codes by category

--- app/controllers/moments.controller.js
+++ app/controllers/moments.controller.js
@@ -7,9 +7,24 @@ app.moment.controller = {
 
     render: function(moment) {
 
-      $("#moment_list").append("<li>" + moment.title + "</li>");
+      if(moment.category ==="Social"){
+        $("#moment_list").append('<li><div class="panel panel-primary"> <div class="panel-body">' + moment.title + '</div> <div class="panel-footer">' + moment.description + "</div> </div></li>")
+      }else if(moment.category ==="Entertainment"){
+         $("#moment_list").append('<li><div class="panel panel-danger"> <div class="panel-body">' + moment.title + '</div> <div class="panel-footer">' + moment.description + "</div> </div></li>")
+      } else if(moment.category === "Work"){
+         $("#moment_list").append('<li><div class="panel panel-warning"> <div class="panel-body">' + moment.title + '</div> <div class="panel-footer">' + moment.description + "</div> </div></li>")
+      } else if(moment.category === "Wellness"){
+         $("#moment_list").append('<li><div class="panel panel-info"> <div class="panel-body">' + moment.title + '</div> <div class="panel-footer">' + moment.description + "</div> </div></li>")
+      } else{
+         $("#moment_list").append('<li><div class="panel panel-default"> <div class="panel-body">' + moment.title + '</div> <div class="panel-footer">' + moment.description + "</div> </div></li>")
+      }
+      // $("#moment_list").append("<li>" + moment.title + "</li>");
+      $("#moment_title").val("");
+      $("#category_dropdown").text("Pick one");
       $("#moment_description").val("");
       $("#address").val("");
     }
   }
 }
+
+
